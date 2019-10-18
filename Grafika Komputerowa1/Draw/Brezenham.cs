d43d3068a989a5754b565d9abf25ce2a35e1bf67
Piotr S <piotr.samborski007@gmail.com>
2019-10-18 12:01:15 +0200

    brezenham working

--- Grafika Komputerowa1/Draw/Brezenham.cs
+++ Grafika Komputerowa1/Draw/Brezenham.cs
@@ -14,45 +14,29 @@ namespace Grafika_Komputerowa1.Draw
         {
             //g.DrawLine(pen, start.x, start.y, end.x, end.y);
             SolidBrush brush = new SolidBrush(Color.Black);
-            //g.SymmetricLine(brush, start.x, start.y, end.x, end.y);
-
-            //if (Math.Abs(end.x - start.x) <= 1)
-            //{
-            //    int yStart = Math.Min(start.y, end.y);
-            //    int yEnd = Math.Max(start.y, end.y);
-            //    int x = end.x;
-            //    for(int y = yStart; y <= yEnd; y++ )
-            //    {
-            //        g.FillRectangle(brush, x, y, 1, 1);
-            //    }
-            //}
-            //else
+            double a = (double)(end.y - start.y) / (end.x - start.x);
+            if (a > 1)
             {
-                double a = (double)(end.y - start.y) / (end.x - start.x);
-                if (a > 1)
-                {
-                    g.MidpointLineN_NE(brush, start.x, start.y, end.x, end.y);
-                    g.MidpointLineN_NE(brush, end.x, end.y, start.x, start.y);
-                }
-                else if (a <= 1 && a > 0)
-                {
-                    g.MidpointLineE_NE(brush, start.x, start.y, end.x, end.y);
-                    g.MidpointLineE_NE(brush, end.x, end.y, start.x, start.y);
-                }
-                else if (a <= 0 && a >= -1)
-                {
-                    g.MidpointLineE_SE(brush, start.x, start.y, end.x, end.y);
-                    g.MidpointLineE_SE(brush, end.x, end.y, start.x, start.y);
-                }
-                else
-                {
-                    //g.MidpointLineS_SE(brush, start.x, start.y, end.x, end.y);
-                    //g.MidpointLineS_SE(brush, end.x, end.y, start.x, start.y);
-                }
+                g.MidpointLineN_NE(brush, start.x, start.y, end.x, end.y);
+                g.MidpointLineN_NE(brush, end.x, end.y, start.x, start.y);
             }
-
+            else if (a <= 1 && a > 0)
+            {
+                g.MidpointLineE_NE(brush, start.x, start.y, end.x, end.y);
+                g.MidpointLineE_NE(brush, end.x, end.y, start.x, start.y);
+            }
+            else if (a <= 0 && a >= -1)
+            {
+                g.MidpointLineE_SE(brush, start.x, start.y, end.x, end.y);
+                g.MidpointLineE_SE(brush, end.x, end.y, start.x, start.y);
+            }
+            else
+            {
+                g.MidpointLineS_SE(brush, start.x, start.y, end.x, end.y);
+                g.MidpointLineS_SE(brush, end.x, end.y, start.x, start.y);
+            }
         }
-        
+
         static void MidpointLineN_NE(this Graphics g, Brush brush, int x1, int y1, int x2, int y2)
         {
             int dy = x2 - x1;
@@ -63,7 +47,7 @@ namespace Grafika_Komputerowa1.Draw
             int x = x1;
             int y = y1;
             g.FillRectangle(brush, x, y, 1, 1);
-            while (x < x2)
+            while (y < y2)
             {
                 if (d < 0) //chooseE 
                 {
@@ -85,7 +69,7 @@ namespace Grafika_Komputerowa1.Draw
             int dy = y2 - y1;
             int d = 2 * dy - dx; //initial value of d 
             int incrE = 2 * dy; //increment used for move to E
-            int incrNE = 2 * (dy-dx);//increment used for move to NE
+            int incrNE = 2 * (dy - dx);//increment used for move to NE
             int x = x1;
             int y = y1;
             g.FillRectangle(brush, x, y, 1, 1);
@@ -143,7 +127,7 @@ namespace Grafika_Komputerowa1.Draw
             int x = x1;
             int y = y1;
             g.FillRectangle(brush, x, y, 1, 1);
-            while (x < x2)
+            while (y2 < y)
             {
                 if (d < 0) //chooseE 
                 {
